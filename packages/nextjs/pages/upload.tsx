48a79f54bb0a5a978090d6f4ad73fb96fd88ad43
robbiekruszynski <robbiekruszynski@gmail..com>
2024-07-13 20:49:05 +0200

    add btn

--- packages/nextjs/pages/upload.tsx
+++ packages/nextjs/pages/upload.tsx
@@ -8,21 +8,35 @@ const Upload: NextPage = () => {
     setText(event.target.value);
   };
 
+  const handleButtonClick = () => {
+    // Add the action you want to trigger here
+    console.log("Button clicked with text:", text);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <input
-        type="text"
-        value={text}
-        onChange={handleTextChange}
-        placeholder="Paste URL here"
-        className="border rounded p-2"
-      />
+      <div className="flex items-center">
+        <input
+          type="text"
+          value={text}
+          onChange={handleTextChange}
+          placeholder="Paste URL here"
+          className="border rounded p-2"
+        />
+        <button
+          onClick={handleButtonClick}
+          className="ml-2 bg-blue-500 text-white rounded p-2"
+        >
+          Enter
+        </button>
+      </div>
       {text && <p className="mt-4">Text entered: {text}</p>}
     </div>
   );
 };
 
 export default Upload;
+
 
 
 
