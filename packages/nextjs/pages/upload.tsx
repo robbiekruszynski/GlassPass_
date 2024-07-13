51938a56437c2289a0047ad9627cd78298c868e5
robbiekruszynski <robbiekruszynski@gmail..com>
2024-07-13 19:42:33 +0200

    upload added, bare

--- packages/nextjs/pages/upload.tsx
+++ packages/nextjs/pages/upload.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from "next";
-
+import { useState } from "react";
 
 const Upload: NextPage = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
   return (
-   <div></div>
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      {file && <p>File selected: {file.name}</p>}
+    </div>
   );
 };
 
