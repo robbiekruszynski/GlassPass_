964a2f49363cf3cd0d62a54db9afa1165fbb05d8
robbiekruszynski <robbiekruszynski@gmail..com>
2024-07-13 20:40:58 +0200

    update pathing

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -30,40 +30,24 @@ const Home: NextPage = () => {
     <>
       {/* <MetaHeader /> */}
       <div className="flex items-center flex-col flex-grow pt-10">
-        {/* <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">GlassPass</span>
-          </h1>
-        </div> */}
-
+        {/* Banner Image fix pathing */}
+        <img src="/public/raave.png" alt="Banner" className="w-full h-auto" />
+        
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-               
-                <Link href="/debug" passHref className="link">
-                Query Location
-                </Link>{" "}
-                
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <SparklesIcon className="h-8 w-8 fill-secondary" />
               <p>
-               
-                <Link href="/example-ui" passHref className="link">
-                  Reserve
+                <Link href="/menu" passHref className="link">
+                  rAAVE
                 </Link>{" "}
-              
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-              
-                <Link href="/activate" passHref className="link">
-                 Activate
+                <Link href="/menu" passHref className="link">
+                 ETHGlobal
                 </Link>{" "}
               </p>
             </div>
@@ -75,3 +59,4 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
--- packages/nextjs/pages/menu.tsx
+++ packages/nextjs/pages/menu.tsx
@@ -1,13 +1,13 @@
-// pages/menu.tsx
 import { useEffect } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { MetaHeader } from "~~/components/MetaHeader";
 import { useGlobalState } from "~~/services/store/store";
 import { web3AuthInstance } from "~~/services/web3/wagmiConnectors";
 
-const Menu: NextPage = () => {
+const Main: NextPage = () => {
   const setUserInfo = useGlobalState(state => state.setUserInfo);
   const { connector } = useAccount();
 
@@ -27,37 +27,51 @@ const Menu: NextPage = () => {
   }, [connector]);
 
   return (
-    <div className="flex items-center flex-col flex-grow pt-10">
-      <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-        <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-          <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <BugAntIcon className="h-8 w-8 fill-secondary" />
-            <p>
-              <Link href="/debug" passHref className="link">
+    <>
+      {/* <MetaHeader /> */}
+      <div className="flex items-center flex-col flex-grow pt-10">
+        {/* <div className="px-5">
+          <h1 className="text-center mb-8">
+            <span className="block text-2xl mb-2">GlassPass</span>
+          </h1>
+        </div> */}
+
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <p>
+               
+                <Link href="/query" passHref className="link">
                 Query Location
-              </Link>
-            </p>
-          </div>
-          <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <SparklesIcon className="h-8 w-8 fill-secondary" />
-            <p>
-              <Link href="/example-ui" passHref className="link">
-                Reserve
-              </Link>
-            </p>
-          </div>
-          <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-            <p>
-              <Link href="/activate" passHref className="link">
-                Activate
-              </Link>
-            </p>
+                </Link>{" "}
+                
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <SparklesIcon className="h-8 w-8 fill-secondary" />
+              <p>
+               
+                <Link href="/reserve" passHref className="link">
+                  Reserve
+                </Link>{" "}
+              
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <p>
+              
+                <Link href="/activate" passHref className="link">
+                 Activate
+                </Link>{" "}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default Menu;
+export default Main;
