b571925e8a50cd659c69ee85344f5f7a243e2b4a
0xglitch <robbiekruszynski@gmail.com>
2024-07-13 15:28:15 +0200

    Initial commit

---
+++ packages/nextjs/pages/index.tsx
@@ -0,0 +1,95 @@
+import { useEffect } from "react";
+import Link from "next/link";
+import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { MetaHeader } from "~~/components/MetaHeader";
+import { useGlobalState } from "~~/services/store/store";
+import { web3AuthInstance } from "~~/services/web3/wagmiConnectors";
+
+const Home: NextPage = () => {
+  const setUserInfo = useGlobalState(state => state.setUserInfo);
+  const { connector } = useAccount();
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      try {
+        if (web3AuthInstance) {
+          const userInfo = await web3AuthInstance.getUserInfo();
+          console.log(userInfo);
+          setUserInfo(userInfo);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUserInfo();
+  }, [connector]);
+
+  return (
+    <>
+      <MetaHeader />
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <h1 className="text-center mb-8">
+            <span className="block text-2xl mb-2">Welcome to</span>
+            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+          </h1>
+          <p className="text-center text-lg">
+            Get started by editing{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/nextjs/pages/index.tsx
+            </code>
+          </p>
+          <p className="text-center text-lg">
+            Edit your smart contract{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              YourContract.sol
+            </code>{" "}
+            in{" "}
+            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+              packages/hardhat/contracts
+            </code>
+          </p>
+        </div>
+
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Tinker with your smart contract using the{" "}
+                <Link href="/debug" passHref className="link">
+                  Debug Contract
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <SparklesIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Experiment with{" "}
+                <Link href="/example-ui" passHref className="link">
+                  Example UI
+                </Link>{" "}
+                to build your own UI.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Explore your local transactions with the{" "}
+                <Link href="/blockexplorer" passHref className="link">
+                  Block Explorer
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ packages/nextjs/services/store/store.ts
@@ -0,0 +1,25 @@
+import { OpenloginUserInfo } from "@web3auth/openlogin-adapter";
+import create from "zustand";
+
+/**
+ * Zustand Store
+ *
+ * You can add global state to the app using this useGlobalState, to get & set
+ * values from anywhere in the app.
+ *
+ * Think about it as a global useState.
+ */
+
+type TGlobalState = {
+  nativeCurrencyPrice: number;
+  setNativeCurrencyPrice: (newNativeCurrencyPriceState: number) => void;
+  userInfo: Partial<OpenloginUserInfo> | null;
+  setUserInfo: (newUserInfo: Partial<OpenloginUserInfo>) => void;
+};
+
+export const useGlobalState = create<TGlobalState>(set => ({
+  nativeCurrencyPrice: 0,
+  setNativeCurrencyPrice: (newValue: number): void => set(() => ({ nativeCurrencyPrice: newValue })),
+  userInfo: null,
+  setUserInfo: (newUserInfo: Partial<OpenloginUserInfo>): void => set(() => ({ userInfo: newUserInfo })),
+}));
